d1540d3dabd32925c3cd782cda34749b99f34031
wildanjr19 <wildansekolah11@gmail.com>
2025-07-14 22:42:59 +0700

    add VAE and main.py

---
+++ VAE/main.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+# Flow Utama
+# Input gambar -> Hidden dim -> mean, std -> Reparameterization trick -> Decoder -> Output gambar
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def encode(self, x):
+        pass
+
+    def decode(self, z):
+        pass
+
+    def forward(self, x):
+        pass
