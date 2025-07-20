31c6a22a9c99180bf0a6fe7856f11d9cdbc54ca3
wildanjr19 <wildansekolah11@gmail.com>
2025-07-20 17:02:11 +0700

    add utils.py

---
+++ Diffusion_Model/utils.py
@@ -0,0 +1,42 @@
+"""
+Script untuk membantu beberapa fungsi :
+- plot
+- simpan gambar
+- praproses data
+"""
+import os
+import torch
+import torchvision
+from PIL import Image
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+def plot_images(images):
+    plt.figure(figsize=(32, 32))
+    plt.imshow(torch.cat([
+        torch.cat([img for img in images.cpu()], dim=1)
+    ], dim=2).permute(1, 2, 0).cpu())
+    plt.show()
+
+def save_images(images, path, **kwargs):
+    grid = torchvision.utils.make_grid(images, **kwargs)
+    ndarray = grid.permute(1, 2, 0).cpu().numpy()
+    im = Image.fromarray(ndarray)
+    im.save(path)
+
+def get_data(args):
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(80),
+        torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    dataset = torchvision.datasets.ImageFolder(args.dataset_path, transform=transforms)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    return dataloader
+
+def setup_logging(run_name):
+    os.makedirs("models", exist_ok=True)
+    os.makedirs("result", exist_ok=True)
+    os.makedirs(os.path.join("models", run_name), exist_ok=True)
+    os.makedirs(os.path.join("result", run_name), exist_ok=True)
