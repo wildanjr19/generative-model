e2cc91167988599ec2dc91b1be6d1eea9405ea3a
wildanjr19 <wildansekolah11@gmail.com>
2025-07-20 16:35:52 +0700

    add SelfAttention block

--- Diffusion_Model/modules.py
+++ Diffusion_Model/modules.py
@@ -23,6 +23,7 @@ class DoubleConv(nn.Module):
         )
 
     def forward(self, x):
+        # apakah residual connection digunakan
         if self.residual:
             return F.gelu(x + self.double_conv(x))
         else:
@@ -49,11 +50,42 @@ class Down(nn.Module):
 
         self.emb_layer = nn.Sequential(
             nn.SiLU(),
+            # proyeksikan dimensi embedding waktu (t)
+            # ke jumlah channel output dari maxpool_conv
             nn.Linear(emb_dim, out_channels)
         )
 
-    def forward(self, x, t):
+    def forward(self, x, t): # t = [batch_size, emb_dim], x = [batch_size, in_channels, height, width]
         x = self.maxpool_conv(x)
-        emb = self.emb_layer(t)[:,:, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        emb = self.emb_layer(t)[:,:, None, None].repeat(1, 1, x.shape[-2], x.shape[-1]) # emb = [batch_size, out_channels, 1, 1] -> [batch_size, out_channels, height, width]
+        # add element-wise
         return x + emb
     
+# attention block
+class SelfAttention(nn.Module):
+    def __init__(self, channels, size):
+        super(SelfAttention, self).__init__()
+        self.channels = channels
+        self.size = size
+        self.mha = nn.MultiheadAttention(channels, num_heads=4, batch_first=True)
+        self.ln = nn.LayerNorm([channels])
+        self.ff_self = nn.Sequential(
+            nn.LayerNorm([channels]),
+            nn.Linear(channels, channels),
+            nn.GELU(),
+            nn.Linear(channels, channels)
+        )
+
+    def forward(self, x):
+        # ubah bentuk agar bisa diproses oleh MHA
+        x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
+        # lewatkan layer norm
+        x_ln = self.ln(x)
+        # MHA
+        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        # skip connection (dengan x awal)
+        attention_value = attention_value + x
+        # lewatakan ke ffn dan skip connection (dengan attention_value)
+        attention_value = self.ff_self(attention_value) + attention_value
+        # ubah kembali bentuknya
+        return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
