a1fe277ba9023eee3d83eb3635e2e2419effcfcb
wildanjr19 <wildansekolah11@gmail.com>
2025-07-21 12:24:57 +0700

    add UNet

--- Diffusion_Model/modules.py
+++ Diffusion_Model/modules.py
@@ -128,4 +128,88 @@ class SelfAttention(nn.Module):
         # ubah kembali bentuknya
         return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
     
-# UNet Block
+# UNet Block -- Backbone of DDPM
+class UNet(nn.Module):
+    def __init__(self, c_in = 3, c_out = 3, time_dim = 256, device = 'cuda'):
+        super(UNet, self).__init__()
+        self.device = device
+        self.time_dim = time_dim
+
+        # PHASE 1 - reduksi
+        """
+        Flow : 
+            Input -> Konvolusi -> Down -> Self Attention 3x
+            ukuran spasial diperkcil
+        """
+        self.inc = DoubleConv(c_in, 64)
+        self.down_1 = Down(64, 128)
+        self.sa_1 = SelfAttention(128, 32)
+        self.down_2 = Down(128, 256)
+        self.sa_2 = SelfAttention(256, 16)
+        self.down_3 = Down(256, 256)
+        self.sa_3 = SelfAttention(256, 8)
+
+        # PHASE 2
+        """
+        Bottleneck -- Deepest part
+        """
+        self.bot_1 = DoubleConv(256, 512)
+        self.bot_2 = DoubleConv(512, 512)
+        self.bot_3 = DoubleConv(512, 256)
+
+        # PHASE 3 - rekonstruksi
+        """
+        Flow :
+            Input -> Up -> Self Attention
+            ukuran spasial diperbesar
+        """
+        self.up_1 = Up(512, 128)
+        self.sa_4 = SelfAttention(128, 16)
+        self.up_2 = Up(256, 64)
+        self.sa_5 = SelfAttention(64, 32)
+        self.up_3 = Up(128, 64)
+        self.sa_6 = SelfAttention(64, 64)
+        self.outc = nn.Conv2d(64, c_out, kernel_size=1)
+
+    # Positional Encoding untuk waktu (t)
+    """
+    Mengubah nilai waktu (t) menjadi vektor embedding berdimensi (channels)
+    Agar model dapat membedakan posisi/timestep unik -- memberitahu model dia sedang bekerja pada waktu berapa
+    """
+    def pos_encoding(self, t, channels):
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device).float() / channels))
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
+    
+    def forward(self, x, t):
+        # tambahkan satu dimensi untuk t ; [batch_size]
+        t = t.unsqueeze(-1).type(torch.float32) # output -> [batch_size, 1]
+        # jadikan t menjadi vektor posisi
+        t = self.pos_encoding(t, self.time_dim) # [batch_size, time_dim]
+
+        # PHASE 1
+        x1 = self.inc(x) # [batch_size, 64, height, width]
+        x2 = self.down_1(x1, t) # [batch_size, 128, height/2, width/2]
+        x2 = self.sa_1(x2)
+        x3 = self.down_2(x2, t) # [batch_size, 256, height/4, width/4]
+        x3 = self.sa_2(x3) 
+        x4 = self.down_3(x3, t) # [batch_size, 256, height/8, width/8]
+        x4 = self.sa_3(x4)
+
+        # PHASE 2
+        x4 = self.bot_1(x4) # [batch_size, 512, height/8, width/8]
+        x4 = self.bot_2(x4) # [batch_size, 512, height/8, width/8]
+        x4 = self.bot_3(x4) # [batch_size, 256, height/8, width/8]
+
+        # PHASE 3
+        x = self.up_1(x4, x3, t) # [batch_size, 128, height/4, width/4]
+        x = self.sa_4(x)
+        x = self.up_2(x, x2, t) # [batch_size, 64, height/2, width/2]
+        x = self.sa_5(x)
+        x = self.up_3(x, x1, t) # [batch_size, 64, height, width]
+        x = self.sa_6(x)
+
+        output = self.outc(x) # [batch_size, c_out, height, width]
+        return output
