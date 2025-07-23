32416be207f9d37a5d2c771b20b840822aa907be
wildanjr19 <wildansekolah11@gmail.com>
2025-07-23 19:59:55 +0700

    add train function

--- Diffusion_Model/ddpm.py
+++ Diffusion_Model/ddpm.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import tqdm as tqdm
 from torch import optim
-# from utils import *
-# from modules import UNet
+from utils import *
+from modules import UNet
 import logging
 from torch.utils.tensorboard import SummaryWriter
 
@@ -88,3 +88,50 @@ class DiffusionModel:
         x = (x * 255).type(torch.uint8)
         
         return x
+
+# training funtion
+def train(args):
+    setup_logging(args.run_name)
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet().to(device)
+    optimizer = optim.AdamW(model.parameters(), lr = args.lr)
+    mse = nn.MSELoss()
+    diffusion = DiffusionModel(img_size=args.image_size, device=device)
+    logger = SummaryWriter(os.path.join("runs", args.run_name))
+    l = len(dataloader)
+
+    for epoch in range(args.epochs):
+        logging.info(f"Starting epoch {epoch} :")
+        pbar = tqdm(dataloader)
+        for i, (images, _) in enumerate(pbar):
+            images = images.to(device)
+            t = diffusion.sample_timesteps(images.size(0)).to(device)
+            x_t, noise = diffusion.noise_images(images, t)
+            predicted_noise = model(x_t, t)
+            loss = mse(predicted_noise, noise)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_postfix(MSE=loss.item())
+            logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+
+        sampled_images = diffusion.sample(model, n=images.shape[0])
+        save_images(sampled_images, os.path.join("runs", args.run_name, f"{epoch}.jpg"))
+        torch.save(model.state_dict(), os.path.join("runs", args.run_name, f"ckpt.pt"))
+
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = "DDPM_Uncondtional"
+    args.epochs = 10
+    args.batch_size = 32
+    args.image_size = 64
+    args.dataset_path = r"D:\Willy's Project\Project\Generative Model\Diffusion_Model\image_data"
+    args.device = "cuda"
+    args.lr = 3e-4
+    train(args)
