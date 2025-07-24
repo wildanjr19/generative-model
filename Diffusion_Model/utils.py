38e6d8005421a8cc6d925c1e10a13a4345cfeb59
wildanjr19 <wildansekolah11@gmail.com>
2025-07-24 07:51:03 +0700

    fix typo

--- Diffusion_Model/utils.py
+++ Diffusion_Model/utils.py
@@ -37,6 +37,6 @@ def get_data(args):
 
 def setup_logging(run_name):
     os.makedirs("models", exist_ok=True)
-    os.makedirs("result", exist_ok=True)
+    os.makedirs("results", exist_ok=True)
     os.makedirs(os.path.join("models", run_name), exist_ok=True)
-    os.makedirs(os.path.join("result", run_name), exist_ok=True)
+    os.makedirs(os.path.join("results", run_name), exist_ok=True)
