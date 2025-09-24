e06e02e4049e2488b91fdc73d2cd0ddb53c8a063
wildanjr19 <wildansekolah11@gmail.com>
2025-09-24 20:10:11 +0700

    add GAN

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -5,4 +5,17 @@ import torchvision
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter # print tensor
+
+class Discriminator(nn.Module):
+    """
+    Bagian diskrimintor dari GAN
+    """
+    def __init__(self, in_features):
+        """
+        Linear -> LeakyReLU -> Linear -> Sigmoid
+        """
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn
+        )
