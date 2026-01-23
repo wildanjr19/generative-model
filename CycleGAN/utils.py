1fe4d9f4628b3018ede178f293efb3532ef21edf
wildanjr19 <wildansekolah11@gmail.com>
2026-01-23 15:50:07 +0700

    save_chechpoint

--- CycleGAN/utils.py
+++ CycleGAN/utils.py
@@ -12,3 +12,11 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
     # get LR
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+def save_checkpoint(model, optimizer, filename = "my_checkpoint.pth.tar"):
+    print("Save Checkpoint...")
+    checkpoint = {
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
